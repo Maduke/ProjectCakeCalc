9648d474f4cd1dd86c606e26d636cc58d172ae24
Davo <rdavo95@gmail.com>
2018-10-31 09:25:49 -0700

    Learning some more

--- venv/CakeCalcMain.py
+++ venv/CakeCalcMain.py
@@ -1,18 +1,40 @@
+from tkinter import *
 
-import tkinter as tk
 
-window = tk.Tk()
+def evntCheck(event=None):
+    print(str(not(boolChk.get())))
+
+
+def evnt_Sub(event):
+    box1 = txtEntry.get()
+    if boolChk.get():
+        box1 = box1.upper()
+    txtOut.delete(0, "end")
+    txtOut.insert(0, box1)
+
+
+window = Tk()
+
+boolChk = BooleanVar()
+boolChk.set(False)
+
 window.title("Custom Creations Cake Calculator")
-mainFrame = tk.Frame(window)
+mainFrame = Frame(window)
 mainFrame.grid(padx=200, pady=5)
 
-tk.Label(mainFrame, text="Hello World").grid(row=0, column=0)
-entry1 = tk.Entry(mainFrame, text='nothing')
-entry1.grid(row=1, column=1)
-tk.Label(mainFrame, text="Hello World").grid(row=1, column=0)
-tk.Label(mainFrame, text="Hello World").grid(row=2, column=0)
-tk.Label(mainFrame, text="Hello World").grid(row=3, column=0)
-tk.Label(mainFrame, text="Hello World").grid(row=4, column=0)
+txtEntry = Entry(mainFrame)
+txtEntry.grid(row=0, column=0)
+
+checkbtn = Checkbutton(mainFrame, text="Upper", variable=boolChk)
+checkbtn.bind("<Button-1>", evntCheck)
+checkbtn.grid(row=1, column=0)
+
+txtOut = Entry(mainFrame)
+txtOut.grid(row=2, column=0)
+
+subBtn = Button(mainFrame, text="Submit")
+subBtn.grid(row=3, column=0)
+subBtn.bind("<Button-1>", evnt_Sub)
 
 
-tk.mainloop()
+window.mainloop()
